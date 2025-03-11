63250216bb18ce9da073923ab4c390d9fe2eb601
Dmitry Chumak <dmchumak@yandex-team.ru>
2025-03-11 12:35:47 +0300

    [nvim] Ctrl-hjkl to move between panes

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -21,3 +21,10 @@ vim.opt.listchars = {
 }
 
 vim.cmd([[match errorMsg /\s\+$/]])
+
+-- Configuring movement between panes
+-- using Ctrl/Cmd-hjkl
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
